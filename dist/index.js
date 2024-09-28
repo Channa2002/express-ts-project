2b1e8475d5963ea29b0f4462ceafe42bf32c0388
Channa2002 <channabasava836@gmail.com>
2024-09-28 21:00:30 +0530

    update

--- dist/index.js
+++ dist/index.js
@@ -6,6 +6,9 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
 const PORT = 3000;
+app.get("/", (req, res) => {
+    res.send("hello");
+});
 app.listen(PORT, () => {
     console.log(`${PORT}`);
 });
