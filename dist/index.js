c7077e9e5bc7d996d42bea210b199d9771ef490c
Channa2002 <channabasava836@gmail.com>
2024-10-02 17:23:59 +0530

    updated work

--- dist/index.js
+++ dist/index.js
@@ -4,16 +4,53 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const mysql2_1 = __importDefault(require("mysql2"));
 const app = (0, express_1.default)();
 app.use(express_1.default.urlencoded({ extended: false }));
 app.use(express_1.default.json());
 const PORT = 3000;
-app.get('/Id/:id/Name/:name', (req, res) => {
-    res.send({
-        message: "hello world",
-        id: req.params.id,
-        name: req.params.name
+app.get('/details/:id', (req, res) => {
+    var pool = mysql2_1.default.createPool({
+        host: "127.0.0.1",
+        user: "root",
+        password: " ",
+        database: "Myconnection",
+        connectionLimit: 10,
+        multipleStatements: true
     });
+    pool.getConnection(function (err, conn) {
+        if (err) {
+            console.log("entered into error");
+            console.log(err);
+            res.send({
+                success: false,
+                statusCode: 500,
+                message: "getting error during the connection"
+            });
+            return;
+        }
+        console.log(`the id: ` + req.params.id);
+        conn.query("SELECT * FROM mydata.actorsdetails_id = ?", [req.params.id], function (err, rows) {
+            if (err) {
+                conn.release();
+                return res.send({
+                    success: false,
+                    statusCode: 400
+                });
+            }
+            res.send({
+                message: "succces",
+                statusCode: 200,
+                data: rows
+            });
+            conn.release();
+        });
+    });
+    // res.send({
+    //     message: "hello world",
+    //     id: req.params.id,
+    //     name: req.params.name
+    // });
 });
 app.post("/Id/:id/Name/:name", (req, res) => {
     res.send({
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
-import express, { Request, Response } from 'express'
+import express, { query, Request, Response } from 'express';
+import mysql2 from "mysql2"
 
 const app = express();
 app.use(express.urlencoded({extended: false}));
@@ -6,12 +7,56 @@ app.use(express.json())
 
 const PORT = 3000;
 
-app.get('/Id/:id/Name/:name', (req: Request, res: Response) => {
-    res.send({
-        message: "hello world",
-        id: req.params.id,
-        name: req.params.name
-    });
+app.get('/details/:id', (req: Request, res: Response) => {
+    
+    var pool = mysql2.createPool({
+        host        : "127.0.0.1",
+        user        : "root",
+        password     : " ",
+        database      : "Myconnection",
+        connectionLimit    : 10,
+        multipleStatements : true
+    })
+
+
+
+    pool.getConnection(function (err: any, conn : any) {
+        if(err) {
+            console.log("entered into error");
+            console.log(err)
+            res.send({
+                success: false,
+                statusCode: 500,
+                message:"getting error during the connection"
+            });
+
+            return;
+        }
+        console.log(`the id: ` + req.params.id);
+        conn.query("SELECT * FROM mydata.actorsdetails_id = ?", [req.params.id], function (err: any, rows : any) {
+            if(err) {
+              conn.release();
+              return res.send ({
+                  success: false,
+                  statusCode: 400
+              });
+            }
+  
+            res.send({
+                message: "succces",
+                statusCode: 200,
+               data : rows
+            });
+  
+            conn.release();
+      })
+    })
+    
+    // res.send({
+    //     message: "hello world",
+    //     id: req.params.id,
+    //     name: req.params.name
+    // });
 })
 
 app.post("/Id/:id/Name/:name", (req: Request, res: Response) => {
